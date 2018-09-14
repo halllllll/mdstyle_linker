0695fa9b5da65acf1859357894cbf8381b8bef52
halllllll <h4l@yahoo.ne.jp>
2018-09-14 13:39:47 +0900

    テストを書いた

--- spec/mdstyle_linker_spec.rb
+++ spec/mdstyle_linker_spec.rb
@@ -1,9 +1,15 @@
+require "mdstyle_linker"
+
 RSpec.describe MdstyleLinker do
   it "has a version number" do
     expect(MdstyleLinker::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context "Normal processing" do
+    it "can generate [title](url) by URL" do
+      url = "https://en.wikipedia.org/wiki/Ruby_(programming_language)"
+      ans = "[Ruby (programming language) - Wikipedia](https://en.wikipedia.org/wiki/Ruby_(programming_language))"
+      expect(MdstyleLinker.mdstyle(url)).to eq ans
+    end
   end
 end
